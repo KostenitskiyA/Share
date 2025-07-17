c81b8b9348abe532916e9e95e170229e03abc463
Kostenitskiy Alexander <kostenitskiy.alexander.work@gmail.com>
2025-07-17 16:33:26 +0300

    Init

---
+++ src/Share/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Share.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static T ConfigureOptions<T>(this IServiceCollection services, IConfiguration configuration) where T : class
+    {
+        var sectionName = typeof(T).Name;
+        var section = configuration.GetSection(sectionName);
+
+        if (!section.GetChildren().Any())
+            throw new Exception($"{sectionName} section is missing or empty");
+
+        var options = section.Get<T>();
+
+        if (options is null)
+            throw new Exception($"{sectionName} options not found");
+
+        services.Configure<T>(section);
+
+        return options;
+    }
+}
