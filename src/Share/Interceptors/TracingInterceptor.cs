524c344d06854fc4613d75b962e3d5665a0510a4
Kostenitskiy Alexander <kostenitskiy.alexander.work@gmail.com>
2025-07-22 07:21:43 +0300

    Update

---
+++ src/Share/Interceptors/TracingInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.Options;
+using Share.Options;
+
+namespace Share.Interceptors;
+
+public class TracingInterceptor(IOptions<OpenTelemetryConfiguration> options) : IInterceptor
+{
+    private readonly ActivitySource ActivitySource = new(options.Value.ServiceName);
+
+    public void Intercept(IInvocation invocation)
+    {
+        using var activity = ActivitySource.StartActivity(invocation.Method.Name);
+        invocation.Proceed();
+    }
+}
---
+++ src/Share/Options/OpenTelemetryConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Share.Options;
+
+public record OpenTelemetryConfiguration
+{
+    public required string ServiceName { get; init; }
+
+    public required string LokiUrl { get; init; }
+
+    public required string OtelUrl { get; init; }
+}
