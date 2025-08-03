c36fd83af3b8cfaa13dce6e17982ff61d41e4e55
Kostenitskiy Alexander <kostenitskiy.alexander.work@gmail.com>
2025-08-03 11:18:25 +0300

    Update

--- src/Share/Extensions/ConfigurationExtensions.cs
+++ src/Share/Extensions/ConfigurationExtensions.cs
@@ -5,18 +5,37 @@ namespace Share.Extensions;
 
 public static class ConfigurationExtensions
 {
+    public static IConfigurationSection GetSection<T>(
+        this IConfiguration configuration)
+        where T : class
+    {
+        var section = configuration.GetSection(typeof(T).Name);
+
+        if (!section.Exists() || !section.GetChildren().Any())
+            throw new InvalidOperationException($"{typeof(T).Name} section is missing or empty");
+
+        return section;
+    }
+
+    public static T GetOptions<T>(
+        this IConfigurationSection configurationSection)
+        where T : class
+    {
+        var options = configurationSection.Get<T>();
+
+        if (options is null)
+            throw new InvalidOperationException($"{typeof(T).Name} options not found");
+
+        return options;
+    }
+
     public static T ConfigureOptions<T>(
         this IServiceCollection services,
         IConfigurationSection section,
         string? name = null)
         where T : class
     {
-        if (!section.Exists() || !section.GetChildren().Any())
-            throw new InvalidOperationException($"{typeof(T).Name} section is missing or empty");
-
-        var options = section.Get<T>();
-        if (options is null)
-            throw new InvalidOperationException($"{typeof(T).Name} options not found");
+        var options = section.GetOptions<T>();
 
         if (string.IsNullOrEmpty(name))
             services.Configure<T>(section);
@@ -32,8 +51,7 @@ public static class ConfigurationExtensions
         string? name = null)
         where T : class
     {
-        var section = configuration.GetSection(typeof(T).Name);
-
+        var section = configuration.GetSection<T>();
         var options = services.ConfigureOptions<T>(section, name);
 
         return options;
